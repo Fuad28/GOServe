a71c06ceb28e029138e82b34ec6c8931ef242a94
Fuad <fuadadio75@gmail.com>
2024-07-05 10:04:48 +0100

    feat: implements basci response

--- app/server.go
+++ app/server.go
@@ -14,10 +14,13 @@ func main() {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
-	
-	_, err = l.Accept()
+
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	response := "HTTP/1.1 200 OK\r\n\r\n"
+	conn.Write([]byte(response))
 }
