09168ae233ceaf5e608fa498a2050e71d447697f
Fuad <fuadadio75@gmail.com>
2024-08-15 00:32:27 +0100

    feat: adds methods to cast KeyStoreValues to different types

--- goserve/utils/key_value_store.go
+++ goserve/utils/key_value_store.go
@@ -10,7 +10,7 @@ type KeyValueStore[K comparable, T any] struct {
 }
 
 // NewKeyValueStore initializes and returns a pointer to a new KeyValueStore instance.
-func NewKeyValueStore[K comparable, T any]() *KeyValueStore[K, T] {
+func NewKeyValueStore[K comparable, T comparable]() *KeyValueStore[K, T] {
 	return &KeyValueStore[K, T]{
 		data: make(map[K]T),
 	}
@@ -41,4 +41,44 @@ func (kv *KeyValueStore[K, T]) Has(key K) bool {
 // GetAll returns all key-value pairs.
 func (kv *KeyValueStore[K, T]) GetAll() map[K]T {
 	return kv.data
+}
+
+// GetString returns the value as string.
+func (kv *KeyValueStore[K, T]) GetString(key K) (typedVal string) {
+	if val, exists := kv.Get(key); exists {
+		typedVal, _ = any(val).(string)
+	}
+	return
+}
+
+// GetInt returns the value as int.
+func (kv *KeyValueStore[K, T]) GetInt(key K) (typedVal int) {
+	if val, exists := kv.Get(key); exists {
+		typedVal, _ = any(val).(int)
+	}
+	return
+}
+
+// GetBool returns the value as bool.
+func (kv *KeyValueStore[K, T]) GetBool(key K) (typedVal bool) {
+	if val, exists := kv.Get(key); exists {
+		typedVal, _ = any(val).(bool)
+	}
+	return
+}
+
+// GetFloat64 returns the value as float64.
+func (kv *KeyValueStore[K, T]) GetFloat64(key K) (typedVal float64) {
+	if val, exists := kv.Get(key); exists {
+		typedVal, _ = any(val).(float64)
+	}
+	return
+}
+
+// GetFloat32 returns the value as float32.
+func (kv *KeyValueStore[K, T]) GetFloat32(key K) (typedVal float32) {
+	if val, exists := kv.Get(key); exists {
+		typedVal, _ = any(val).(float32)
+	}
+	return
 }
